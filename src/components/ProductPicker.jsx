a76d6e9ca7526d790dc6b21ec8edd05f6d436d65
ashwikul <ashwinikk1990@gmail.com>
2024-09-27 07:19:55 +0530

    add initial api call and display products in product picker

--- src/components/ProductPicker.jsx
+++ src/components/ProductPicker.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import close from "../assets/close.svg";
 import search from "../assets/search.svg";
 
 function ProductPicker({ setProductPicker }) {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [productsList, setProductsList] = useState([]);
+
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+
+  const fetchInitialData = async () => {
+    const res = await fetch(
+      `https://stageapi.monkcommerce.app/task/products/search?page=0&limit=10`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": apiKey, // Authorization header
+        },
+      }
+    );
+    const data = await res.json();
+    console.log("data", data);
+    setProductsList(data);
+  };
+
+  const updateProductChecked = (product) => {
+    console.log("product", product);
+  };
+
+  const updateVariantChecked = (variant, product) => {
+    console.log("variant", variant);
+    console.log("product", product);
+  };
+
+  console.log("productsList", productsList);
+
   const handleSearch = (value) => {
     console.log(value);
   };
@@ -15,6 +49,7 @@ function ProductPicker({ setProductPicker }) {
           <button>
             <img
               src={close}
+              alt="close"
               width={17}
               height={17}
               onClick={() => setProductPicker(false)}
@@ -25,6 +60,7 @@ function ProductPicker({ setProductPicker }) {
           <div className="flex relative w-full h-8">
             <img
               src={search}
+              alt="search"
               width={17}
               height={17}
               className="absolute left-4 top-1/2 transform -translate-y-1/2 w-4 h-4"
@@ -38,8 +74,54 @@ function ProductPicker({ setProductPicker }) {
         </nav>
         <div className="flex-grow overflow-auto">
           <ul>
-            <li>product 1</li>
-            <li>product 2</li>
+            {productsList.map((product) => (
+              <li key={product.id} className="border border-[#0000001A] pt-3 ">
+                <div className="flex  items-center gap-2 ml-5">
+                  <input
+                    type="checkbox"
+                    checked={product.checked || false}
+                    className="accent-[#008060]"
+                    onChange={() => updateProductChecked(product)}
+                  />
+                  <img
+                    src={product.image.src}
+                    width={36}
+                    height={36}
+                    alt={product.title}
+                  />
+                  <p>{product.title}</p>
+                </div>
+
+                {product.variants && (
+                  <ul>
+                    {product.variants.map((variant, index) => (
+                      <li
+                        key={variant.id}
+                        className="border border-[#0000001A] py-3  "
+                      >
+                        <div className="flex justify-between ml-12 mr-5">
+                          <div className="flex gap-2">
+                            <input
+                              type="checkbox"
+                              className="accent-[#008060]"
+                              checked={variant.checked || false}
+                              onChange={() =>
+                                updateVariantChecked(variant, product)
+                              }
+                            />
+                            <p> {variant.title}</p>
+                          </div>
+                          <div className="flex gap-2">
+                            <p>{variant.product_id} available</p>
+                            <p>${variant.price}</p>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
           </ul>
         </div>
         <footer className="flex justify-between items-center py-3 px-5  sticky bottom-0 h-12 border border-[#0000001A]">
